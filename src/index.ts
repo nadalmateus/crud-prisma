79657254d5bfef5cac59f83d0ff2e3db09b38f90
Mateus Nadal <nadalmateus@hotmail.com>
2021-10-25 23:48:33 -0300

    feat(byid): find by id route

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,18 @@ const prisma = new PrismaClient()
 app.get('/', async (request: Request, response: Response) => {
   const users = await prisma.user.findMany()
 
-  return response.json({ users })
+  return response.json(users)
+})
+
+app.get('/byId/:id', async (request: Request, response: Response) => {
+  const id = request.params.id
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(id)
+    }
+  })
+
+  return response.json(user)
 })
 
 app.post('/', async (request: Request, response: Response) => {
@@ -20,7 +31,7 @@ app.post('/', async (request: Request, response: Response) => {
       password: password
     }
   })
-  return response.json({ user })
+  return response.json(user)
 })
 
 app.put('/', async (request: Request, response: Response) => {
@@ -33,7 +44,7 @@ app.put('/', async (request: Request, response: Response) => {
       username: username
     }
   })
-  return response.json({ updatedUser })
+  return response.json(updatedUser)
 })
 
 app.delete('/:id', async (request: Request, response: Response) => {
@@ -43,7 +54,7 @@ app.delete('/:id', async (request: Request, response: Response) => {
       id: Number(id)
     }
   })
-  return response.json({ deletedUser })
+  return response.json(deletedUser)
 })
 
 app.listen(3333, () => { console.log('3333') })
